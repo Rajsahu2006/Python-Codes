08b2a3187a474012ab15999be2e70ded0a0bf09c
Rajsahu2006 <rajsahu062006@gmail.com>
2026-09-07 11:11:16 +0530

    Add files via upload

---
+++ L3.py
@@ -0,0 +1,31 @@
+import pandas
+print("hello\n I am a RAJ")
+#\n ka use sentense ko next line ma pprint karne ke liya hota ha 
+print("Hey" ,2,3,4,sep="__" , end="94u8t\n")
+
+#sep = Seprater
+
+#data Type 
+
+a = 234 #integer
+b = 2.43  #float
+c = "Raaaj" #String
+d = None  #None
+e = complex( 2,6)  #complex no.
+
+print("The type of a is", type(a))
+print("The type of b is", type(b))
+print("The type of c is", type(c))
+print("The type of d is", type(d))
+print("The type of e is", type(e))
+
+#List and Tupple
+
+List1 = [1,2,3,4,5,65],["Apple","Banana","Graphs"]
+print(List1)
+
+tuple = [0,9,8,76,]
+print(tuple)
+
+dict = ["red","Black","White","Green"]
+print(dict)   #collection of key value of pairs
